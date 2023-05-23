b1675c0ab5b7b4538dae548a77a2b3337f03db38
ThaiBao <thaibao203.1987@gmail.com>
2023-05-24 01:42:04 +0700

    add IO

--- lapLichCPU.cpp
+++ lapLichCPU.cpp
@@ -6,78 +6,146 @@ private:
     int id; // process id
     int cpu; // cpu time burst
     int arrival; // time arrival in cpu
+    int io; // I/O time burst
+    int ioStart; // time start I/O
+    bool haveIO;
 public:
     Process(){
         id = 0;
         cpu = 0;
         arrival = 0;
+        io = -1;
+        ioStart = -1;
+        haveIO = false;
     }
-    Process(int _id, int _cpu, int _arrival){
+    Process(int _id, int _cpu, int _arrival, int _io, int _ioTime, bool _haveIO){
         id= _id;
         cpu = _cpu;
         arrival = _arrival;
+        haveIO = _haveIO;
+        if(haveIO){
+            io = _io;
+            ioStart = _ioTime;
+        }
+        else{
+            io = -1;
+            ioStart = -1;
+        }
     }
 
-    void printGantt(Process p[], int n){
-        Process process[n];
-        vector<Process> CPU; // CPU queue
-        vector<Process> orderCPU;
-        char cpu[n][50];
+    void printGantt(Process p[], int n, bool IO){
+        vector<Process> CPU; // CPU run
+        vector<Process> ioRun; // IO run
+        vector<Process> cpuQueue, ioQueue; // CPU queue | IO queue
+        char cpu[n][50], io[n][50];
         int currTime=0;
         int cnt=0;
         int wt[n];
-
-        for(int i=0; i<n; i++){
-            process[i]=p[i];
-        }
 
         // set up the array
         for(int i=0; i<n; i++){
             wt[i]=0;
             for(int j=0; j<50; j++){
                 cpu[i][j]='-';
+                io[i][j]='-';
             }
         }
        
         // browse each colume with current time = 0
-        for(currTime=0; cnt<n || (!CPU.empty() || !orderCPU.empty()); currTime++){  
+        for(currTime=0; cnt<n || !CPU.empty() || !cpuQueue.empty() || !ioRun.empty() || !ioQueue.empty(); currTime++){  
             // if arrive at current time
             for(int i=0; i<n; i++){
                 if(p[i].arrival==currTime){
-                    orderCPU.push_back(p[i]);
+                    cpuQueue.push_back(p[i]);
                     cnt++;
                 }
             }
-            // if in CPU has a process that reach at its end time
-            if(!CPU.empty()){
-                if(CPU[0].arrival+CPU[0].cpu==currTime)
-                    CPU.erase(CPU.begin());
-            }
-            // output | for CPU doing nothing
-            if(orderCPU.empty() && CPU.empty()){
-                for(int i=0; i<n; i++){
-                    cpu[i][currTime]='|';
-                }
-                continue;
-            }
             // CPU doesn't have any process, push process from cpu queue to run
-            if(!orderCPU.empty() && CPU.empty()){
-                CPU.push_back(orderCPU[0]);
-                orderCPU.erase(orderCPU.begin());
+            while(!cpuQueue.empty() && CPU.empty()){
+                CPU.push_back(cpuQueue[0]);
+                if(CPU[0].arrival+CPU[0].ioStart==currTime){
+                    ioQueue.push_back(CPU[0]);
+                    CPU.clear();
+                }
+                cpuQueue.erase(cpuQueue.begin());
             }
-            // 1 process take CPU and other processes in CPU queue
-            if(!orderCPU.empty() && !CPU.empty()){
-                for(int i=0; i<orderCPU.size(); i++){
-                    wt[orderCPU[i].id-1]++;
-                    orderCPU[i].arrival++;
+
+            if(!CPU.empty()){
+                if(CPU[0].arrival+CPU[0].ioStart==currTime){
+                    ioQueue.push_back(CPU[0]);
+                    CPU.clear();
                 }
             }
-            // output # for process running CPU
+
+            if(!ioRun.empty()){
+                if(ioRun[0].arrival+ioRun[0].ioStart+ioRun[0].io==currTime){
+                    
+                    ioRun[0].cpu+=ioRun[0].io;
+                    cpuQueue.push_back(ioRun[0]);
+                    ioRun.clear();
+                }
+            }
+            
+            if(ioRun.empty()){
+                if(!ioQueue.empty()){
+                    //cout<<ioQueue[0].arrival<<'\n';
+                    ioRun.push_back(ioQueue[0]);
+                    ioQueue.erase(ioQueue.begin());
+                }
+            }
+            if(!ioRun.empty()){
+                if(!ioQueue.empty()){
+                    for(int i=0; i<ioQueue.size(); i++){
+                        ioQueue[i].io++;
+                    }
+                }
+            }
+            if(!CPU.empty()){
+                if(CPU[0].arrival+CPU[0].cpu==currTime){
+                    CPU.clear();
+                }
+            }
+            
+            // CPU doesn't have any process, push process from cpu queue to run
+            if(!cpuQueue.empty() && CPU.empty()){
+                CPU.push_back(cpuQueue[0]);
+                cpuQueue.erase(cpuQueue.begin());
+            }
+
+            // 1 process take CPU and other processes in CPU queue
+            if(!cpuQueue.empty() && !CPU.empty()){
+                for(int i=0; i<cpuQueue.size(); i++){
+                    wt[cpuQueue[i].id-1]++;
+                    cpuQueue[i].arrival++;
+                }
+            }
+            if(!CPU.empty()){
+                if(CPU[0].arrival+CPU[0].cpu==currTime){
+                    CPU.clear();
+                }
+            }
+            if(CPU.empty()){
+                if(cpuQueue.empty()){
+                    for(int i=0; i<n; i++){
+                        cpu[i][currTime]='|';
+                    }
+                }
+            }
             if(!CPU.empty()){
                 cpu[CPU[0].id-1][currTime]='#';
             }
+            if(ioRun.empty()){
+                if(ioQueue.empty())
+                    for(int i=0; i<n; i++){
+                        io[i][currTime]='|';
+                    }
+            }
+
+            if(!ioRun.empty()){
+                io[ioRun[0].id-1][currTime]='#';
+            }
         }
-        cout<<'\n';
+        cout<<"CPU: \n";
         for(int i=0; i<n; i++){
             cout<< "Process "<<i+1<<": ";
             for(int j=0; j<currTime; j++){
@@ -85,42 +153,27 @@ public:
             }
             cout<<'\n';
         }
-        cout<<'\n';
-        double waitingTime=0;
-        double turnAround=0;
-        double CPUusage=0;
-        for(int i=0; i<currTime-1; i++){
-            if(cpu[0][i]!='|')
-                CPUusage++;
-        }
-        for(int i=0; i<n; i++)
-            waitingTime+=double(wt[i]);
-        cout<<"CPU usage: "<<CPUusage*100/(currTime-1)<<"%\n";
-        cout<<"Average waiting time: "<<waitingTime/n<<'\n';
-        cout<<"Average response time: "<<waitingTime/n<<'\n';
+        cout<<"IO: \n";
         for(int i=0; i<n; i++){
-            int tmp=0;
-            bool flag=false;
-            for(int j=0; j<currTime; j++){ 
-                while(cpu[i][j]=='#'){
-                    j++;
-                    tmp=j;
-                    flag=true;
-                }
-                if (flag)
-                    break;
+            cout<< "Process "<<i+1<<": ";
+            for(int j=0; j<currTime; j++){
+                cout<<io[i][j];
             }
-            turnAround+=tmp-process[i].arrival;
+            cout<<'\n';
         }
-        cout<<"Average turn-around time: "<<double(turnAround)/n<<'\n';
     }
 };
 
 int main(){
     Process pArr[100], p;
-    int n, cpu, arrival;
+    int n, x, cpu, arrival, io=-1, ioStart=-1;
+    bool IO = false;
     cout << "Enter number of processes: ";
     cin >> n;
+    cout << "\nDo the processes have IO? (note: 1 mean 'yes', 0 mean 'no') ";
+    cin >> x;
+    if(x == 1)
+        IO=true;
 
     for(int i=0; i<n; i++){
         cout << "\nProcess: " << i+1<<'\n';
@@ -128,9 +181,16 @@ int main(){
         cin >> cpu;
         cout << "===> Arrival time: ";
         cin >> arrival;
-        Process process(i+1, cpu, arrival);
+        if(IO){
+            cout <<"===> IO burst time: ";
+            cin >> io;
+            cout <<"===> IO start time: ";
+            cin >> ioStart;
+        }
+        Process process(i+1, cpu, arrival, io, ioStart, IO);
         pArr[i] = process;
     }
 
-    p.printGantt(pArr, n);
+    p.printGantt(pArr, n, IO);
 }
+
